53e3d4f502fa91bb34ba2c92371b7562c93f9673
David <mcdave91@gmail.com>
2013-02-27 19:40:59 +0100

    Update executePhoto.sql

--- executePhoto.sql
+++ executePhoto.sql
@@ -1,9 +1,3 @@
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for `list_clients`
--- ----------------------------
-DROP TABLE IF EXISTS `list_clients`;
 CREATE TABLE `list_clients` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `ip` varchar(120) DEFAULT NULL,
@@ -13,4 +7,4 @@ CREATE TABLE `list_clients` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ip` (`ip`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
